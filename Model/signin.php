ff573d6a78401ed08496ed6a6ebb752b8fbe980d
Othman S Othman <67808289+OthmanSO@users.noreply.github.com>
2022-04-19 23:05:23 +0300

    Update signin.php

--- Model/signin.php
+++ Model/signin.php
@@ -7,27 +7,25 @@ $db = "EmployeesDB";
 
 $con = new PDO("mysql:host={$hostname};dbname={$db}", $username, $password);
 
-if(isset($_REQUEST['']))
+if(!isset($_REQUEST['ID']) || !isset($_REQUEST['password'])){
+    echo 'NO USER!';
+    return;
+}
 
+$usrID = $_REQUEST['ID'];
+$usrPswd = sha1($_REQUEST['password']);
 
-
-$sql2 = "SELECT * FROM EMPLOYEE WHERE ID = 0";
+$sql2 = "SELECT * FROM EMPLOYEE WHERE ID =" . $usrID . " AND password =" .$usrPswd;
 $stmt = $con->prepare( $sql2 );
 $stmt->execute();
 $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-if (isset($_GET['startFrom'])){
-    $count = 0;
-    while ( $row != null && $count < $_GET['startFrom']){
-        $count++;
-        $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    }
-}
-$count = 0;
-while ( $row != null && $count < 5){
-    echo "|&&|" . $row["ID"] . "|&|" . $row["Name"]. "|&|" . $row["Address"]. "|&|" . $row["Email"]. "|&&|" ;
-    $count++;
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-}
+
+if( $row != null ){
+    if ($row['adminflag'] == 1) echo "ADMIN";
+    else echo "EMPLOYEE";
+    echo "|&|" . $row["ID"] . "|&|" . $row["Name"]. "|&|" . $row["Address"]. "|&|" . $row["Email"] ;
+} 
+else echo'NO USER FOUND!';
 
 $con = null;
